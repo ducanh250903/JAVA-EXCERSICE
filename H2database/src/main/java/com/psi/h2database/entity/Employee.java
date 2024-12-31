46d0dd06851a631fc400f296582b63a51d87b0c9
Lê Đức Anh <6651649@sv.vnua.edu.vn>
2024-12-31 10:55:22 +0700

    commit h2database

---
+++ H2database/src/main/java/com/psi/h2database/entity/Employee.java
@@ -0,0 +1,9 @@
+package com.psi.h2database.entity;
+
+public class Employee {
+    private Integer emp_id;
+    private String emp_name;
+    private String emp_position;
+    private Double emp_salary;
+    private Integer emp_department;
+}
